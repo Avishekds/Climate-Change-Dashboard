8ec157721221c3d3905c21d87d01b55dfcd2c37b
Avishek Das <69322783+Avishekds@users.noreply.github.com>
2022-04-30 14:24:37 +0530

    Update Climate.py

--- Climate.py
+++ Climate.py
@@ -10,7 +10,7 @@ import plotly.graph_objs as go
 from plotly.offline import init_notebook_mode,iplot,plot
 init_notebook_mode(connected=True)
 import plotly.express as px
-from raceplotly.plots import barplot
+
 
 
 
